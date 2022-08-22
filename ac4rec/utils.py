6d6f4122ea206498f5050bb81d99bea0fb1a4753
welch <welch_cn@163.com>
2022-08-22 15:56:24 +0800

    创建项目

---
+++ ac4rec/utils.py
@@ -0,0 +1,230 @@
+import numpy as np
+import torch.nn as nn
+import torch
+import math
+import heapq
+import random
+from torch.distributions import Categorical
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+class Voc:
+    def __init__(self, sentence=False):
+        self.word2index = {}
+        self.word2count = {}
+        self.index2word = {}
+        self.num_words = 0
+        if type(sentence) != type(False):
+            self.addSentence(sentence)
+
+    def addSentence(self, sentence):
+        for word in sentence:
+            self.addWord(word)
+
+    def addWord(self, word):
+        if word not in self.word2index:
+            self.word2index[word] = self.num_words
+            self.word2count[word] = 1
+            self.index2word[self.num_words] = word
+            self.num_words += 1
+        else:
+            self.word2count[word] += 1
+
+class DataPre:
+    def __init__(self, data):
+        self.data = data
+        self.itemPrice = None  # item 平均出价金额作为item的price
+        self.userBudgets = None  # 根据用户交互的item的平均price作为用户的budget
+        self.userVoc = Voc(data['商户ID'])
+        self.itemVoc = Voc(data['车ID'])
+        self.actionVoc = Voc(data['是否中标'])
+        self.item_freq = {} # {item_id: frequence}
+        self.seq = {}  # item_seq {uid: {[iid, action, value],[...]}}
+        self._toSeq()
+        self._getItemPrice()
+        self._getUserBudgets()
+
+    def _toSeq(self):
+        for index, row in self.data.iterrows():
+            uid = self.userVoc.word2index[int(row['商户ID'])]
+            iid = self.itemVoc.word2index[int(row['车ID'])]
+            aid = self.actionVoc.word2index[int(row['是否中标'])]
+            self.seq.setdefault(uid, [])
+            self.item_freq.setdefault(iid, 0)
+            self.seq[uid].append([iid, aid, round(float(row['出价金额']), 2), int(row['出价时间'])])
+            self.item_freq[iid] += 1
+
+            # making order meanful and thus can remove time_stamp
+        for uid, items in self.seq.items():
+            self.seq[uid] = sorted(items, key=lambda x: x[-1])
+
+    def _getItemPrice(self):
+        itemPrice = [0]*self.itemVoc.num_words
+        for word in self.itemVoc.word2index.keys():
+            temp = self.data[self.data["车ID"] == word]
+            if temp.count()['车ID']>3:
+                bid_mean =(temp["出价金额"].sum() - (temp["出价金额"].max() + temp["出价金额"].min())) / (temp.count()['车ID'] - 2)
+            else:
+                bid_mean = temp["出价金额"].mean()
+            item_idx = self.itemVoc.word2index[word]
+            itemPrice[item_idx] = int(bid_mean)
+        self.itemPrice = itemPrice
+
+    def _getUserBudgets(self):
+        userBudgets = [0] * self.userVoc.num_words
+        for key, values in self.seq.items():
+            provide_prices = [p[2] for p in values]
+            if len(values) > 3:
+                userBudgets[key] = int((sum(provide_prices)-max(provide_prices)-min(provide_prices))/(len(provide_prices)-2))
+            else:
+                userBudgets[key] = int(sum(provide_prices)/len(provide_prices))
+        self.userBudgets = userBudgets
+
+class Policy(nn.Module):
+    def __init__(self, input_dim):
+        super(Policy, self).__init__()
+        self.fc = nn.Sequential(
+            nn.Linear(input_dim, int(0.5 * input_dim)),
+            nn.ReLU(),
+            nn.Linear(int(0.5 * input_dim), 2),
+            nn.Softmax()
+        )
+    def forward(self, input):
+        """
+        根据当前状态
+        :return: [0~1]
+        """
+        out = self.fc(input)
+        return out
+# 预测用户的budget
+class BudgetNet(nn.Module):
+    # 以用户embedding初始化h_0
+    # 以上一时刻的item embeddin作为输入
+    # 预测下一时刻的budget
+    def __init__(self, user_num, user_dim, item_num, item_dim, budget_dim, gru_hidden_size):
+        super(BudgetNet, self).__init__()
+        # item嵌入矩阵
+        self.item_em = nn.Embedding(item_num, item_dim).to(device)
+
+        # user 嵌入矩阵item
+        self.user_em = nn.Embedding(user_num, user_dim).to(device)
+
+        self.gru = nn.GRUCell(input_size=item_dim, hidden_size=gru_hidden_size)
+
+        # self.gru = nn.GRU(input_size=item_dim, hidden_size=gru_hidden_size)
+        self.fc = nn.Sequential(
+            nn.Linear(gru_hidden_size, int(0.5*gru_hidden_size)),
+            nn.Linear(int(0.5*gru_hidden_size), budget_dim)
+        )
+
+    def forward(self, item_id, user_id):
+        """
+        """
+        # [user_dim]
+        user_em = self.user_em(torch.LongTensor([user_id]).to(device))
+        # [seq_len, item_dim]
+        item_em = self.item_em(torch.LongTensor([item_id]).to(device))
+
+        # 初始化隐藏状态
+        # [seq_len, gru_hidden_size]
+        out = self.gru(item_em)
+        # [seq_len, budget_dim]
+        budget_pred = self.fc(out)
+        # [seq_len, budget_dim]
+        return budget_pred+user_em
+
+def item_split(items_price, step):
+    num_items = len(items_price)
+    indices = list(range(num_items))
+    splited_item = []
+    for i in range(0, num_items, step):
+        ids = indices[i: min(i + step, num_items)]
+        splited_item.append([items_price[i] for i in ids])
+    return splited_item
+
+def data_split(data, rate=0.8):
+    train_data = {}
+    eval_data = {}
+    for key, value in data.items():
+        if len(value)*rate < 2 or len(value)*(1-rate) < 2:
+            pass
+        train_data[key] = value[:int(len(value)*rate)]
+        eval_data[key] = value[int(len(value)*rate):]
+    return train_data, eval_data
+
+# 选择 action 计算对应的概率
+def action_select(state, action_num, policys):
+    directs = [p(state).squeeze() for p in policys]  # 计算policy的选择概率
+    # print(f"action_select: {directs}")
+    p = 1
+    left = 0
+    right = action_num - 1
+    mid = int((left + right) / 2)
+    for i in range(len(directs)):
+        max_idx = category_sampling(directs[i])
+        # max_idx = torch.argmax(directs[i]).item()
+        p = p * directs[i][max_idx]
+        if max_idx == 1:  # 向右
+            left = mid + 1
+        else:  # 向左
+            right = mid
+        mid = int((left + right) / 2)
+
+        if left == right:  # 搜索到
+            return left, p  # 返回选择的action以及对应的概率
+
+def action_distribution(state, action_num, policys, prob = None):
+    """
+    :param budget: budget状态向量
+    :param item_num: 选择的budget_block对应的item_num
+    :param item_polocys: 选择策略
+    :return:
+    """
+    directs = [p(state).squeeze() for p in policys]  # 计算policy的选择概率
+    # print(f"action_distribution: {directs}")
+    item_in_block_dist = [0] * action_num
+    for target in range(action_num):
+        if prob is None:
+            p = 1
+        else:
+            p = prob
+        left = 0
+        right = action_num - 1
+        mid = int((left + right) / 2)
+        layer = 0
+        while 1:
+            if target <= mid:  # 向左
+                right = mid
+                p = p * directs[layer][0]
+            else:  # 向右
+                left = mid + 1
+                p = p * directs[layer][1]
+
+            mid = int((left + right) / 2)
+
+            if left == target and right == target:
+                item_in_block_dist[target] = p.unsqueeze(dim=0)
+                break
+            else:
+                layer += 1  # 开始下层搜索
+
+    return item_in_block_dist
+
+def data_loader(uids, seq, shuffle, batch_size):
+    num_examples = len(seq)
+    indices = list(range(num_examples))
+    if shuffle:
+        random.shuffle(indices)  # 样本的读取顺序是随机的
+    for i in range(0, num_examples, batch_size):
+        ids = indices[i: min(i + batch_size, num_examples)]  # 最后⼀次可能不⾜⼀个batch
+        yield [uids[j] for j in ids], [torch.tensor(seq[j][:-1]) for j in ids], [torch.tensor(seq[j][1:]) for j in ids]
+
+def category_sampling(prob):
+    m = Categorical(prob)
+    return m.sample().item()
+
+if __name__=="__main__":
+    # item_prices = range(50000)
+    state = torch.randn(size=[10], dtype=torch.float32)
+    #
+
---
+++ multi-GRU4Rec.py
@@ -0,0 +1,247 @@
+import sys
+import csv
+import math
+import random
+import copy
+import argparse
+import logging
+import ast
+import datetime
+
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+import numpy as np
+import pandas as pd
+from sklearn import metrics
+from sklearn.model_selection import train_test_split
+from scipy.sparse import coo_matrix
+from tqdm import tqdm
+from sklearn.metrics import roc_auc_score
+
+import torch
+import torch.nn as nn
+from sklearn import metrics
+import torch.nn.functional as F
+import torch.optim as optim
+from torch.utils.data import DataLoader
+from torch.autograd import Variable
+from torch.optim.lr_scheduler import StepLR
+import torch.utils.data
+import torch.utils.data as data
+from torch.nn.utils.rnn import pack_padded_sequence, pad_packed_sequence
+
+dtype = torch.float
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+
+class Voc:
+    def __init__(self, sentence=False):
+        PAD_token = 0  # Used for padding short sentences
+        self.word2index = {}
+        self.word2count = {}
+        self.index2word = {PAD_token: "PAD"}
+        self.num_words = 1
+        if type(sentence) != type(False):
+            self.addSentence(sentence)
+
+    def addSentence(self, sentence):
+        for word in sentence:
+            self.addWord(word)
+
+    def addWord(self, word):
+        if word not in self.word2index:
+            self.word2index[word] = self.num_words
+            self.word2count[word] = 1
+            self.index2word[self.num_words] = word
+            self.num_words += 1
+        else:
+            self.word2count[word] += 1
+
+
+class DataPre:
+    def __init__(self, data):
+        self.data = data
+        self.userVoc = Voc(data['商户ID'])
+        self.itemVoc = Voc(data['车ID'])
+        self.actionVoc = Voc(data['是否中标'])
+        self.seq = {}  ##item_seq {uid: {[iid, action, value],[...]}}
+
+        self._toSeq()
+
+    def _toSeq(self):
+        for index, row in self.data.iterrows():
+            uid = self.userVoc.word2index[int(row['商户ID'])]
+            iid = self.itemVoc.word2index[int(row['车ID'])]
+            aid = self.actionVoc.word2index[int(row['是否中标'])]
+            self.seq.setdefault(uid, [])
+            self.seq[uid].append([iid, aid, row['出价金额'], int(row['出价时间'])])
+
+            # making order meanful and thus can remove time_stamp
+        for uid, items in self.seq.items():
+            self.seq[uid] = sorted(items, key=lambda x: x[-1])
+
+
+class Dataset(torch.utils.data.Dataset):
+    def __init__(self, data_seq):
+        super().__init__()
+        self.data_seq = data_seq
+
+    def __len__(self):
+        return len(self.data_seq)
+
+    def __getitem__(self, idx):
+        return self.data_seq[idx]
+
+
+def collate_fn(data):
+    dataset = [list(map(lambda x: [x[0],x[2]], seq)) for seq in data]  # x[0] stores itemID; x[1] stores 出价金额
+    dataset.sort(key=lambda x: len(x), reverse=True)
+
+    input_len = [len(seq) - 1 for seq in dataset]
+
+    input_ = [seq[:-1] if len(seq[:-1]) == input_len[0] else seq[:-1] + [[0,0]] * (input_len[0] - len(seq[:-1]))
+                 for seq in dataset]
+
+    input_seq_iid = []
+    for item in input_:
+        input_seq_iid.append([i[0] for i in item])
+
+    output_ = [seq[1:] if len(seq[1:]) == input_len[0] else seq[1:] + [[0,0]] * (input_len[0] - len(seq[1:]))
+                  for seq in dataset]
+
+    output_seq_iid = []
+    output_seq_price = []
+    for item in output_:
+        output_seq_iid.append([i[0] for i in item])
+        output_seq_price.append([i[1] for i in item])
+    return input_seq_iid, output_seq_iid, output_seq_price, input_len
+
+
+df = pd.read_csv('dataset/filtered_data.csv')
+basetime = datetime.datetime.strptime(df['出价时间'].min(), '%Y-%m-%d')
+# 将出价时间设置为与basetime的差
+df['出价时间'] = df['出价时间'].apply(lambda x: (datetime.datetime.strptime(x, '%Y-%m-%d') - basetime).days)
+print("row 数目：", len(df["车ID"].tolist()))
+print("车的数目：",len(set(df["车ID"].tolist())))
+print(df['出价金额'].min(), df['出价金额'].max())
+cid_df = df[df["车ID"].isin([1690])]
+df['出价金额'] = df['出价金额'].apply(lambda x: x/100000)
+
+dp = DataPre(df)
+
+from torch.utils.data import DataLoader
+from torch.utils.data import random_split
+
+mydata = Dataset(list(map(lambda x: x[1][-6:], list(dp.seq.items()))))  # last 5 items of each user
+
+# split dataset
+train_size = int(dp.userVoc.num_words * 0.8)
+val_size = int(dp.userVoc.num_words * 0.05)
+test_size = dp.userVoc.num_words - train_size - val_size - 1
+
+train, val, test = random_split(mydata, [train_size, val_size, test_size])  # 不重合
+
+
+class GRU4Rec(nn.Module):
+    def __init__(self, Ni, K, emb_reg=False):
+        super(GRU4Rec, self).__init__()
+        # torch.manual_seed(999)
+        # voc_size
+        self.Ni = Ni
+        # emb_size
+        self.K = K
+        self.emb_reg = emb_reg
+
+        self.i_em = nn.Embedding(Ni, K)
+        self.i_em.weight.data.normal_(0, .1)
+        # emb_size = hidden_size = K
+        self.gru = nn.GRU(K, K, 1, batch_first=True)
+        '''
+        input_size, hidden_size, num_layers
+        '''
+        self.hid2voc = nn.Linear(K, Ni)
+        self.hid2price = nn.Linear(K, 1)
+
+        self.ce = nn.CrossEntropyLoss()
+        self.mse = nn.MSELoss(reduction='mean')
+
+    def forward(self, seq, seq_len):
+        ems = self.i_em(torch.tensor(seq))  # [batch, token_length, K]
+        # padded
+        ems = pack_padded_sequence(ems, seq_len, batch_first=True)
+        # out:PackedSequence
+        out, h = self.gru(ems)
+
+        return self.hid2voc(out.data), self.hid2voc(h), self.hid2price(out.data)
+
+
+def evaluation(model, input_x, output_y, seq_len, K):
+    mrr = 0.
+    hit_n = [0] * K
+
+    out, h, _ = model(input_x, seq_len)
+
+    _, indices = torch.sort(h, descending=True)
+    indices = indices.squeeze(0)
+
+    for i in range(indices.shape[0]):
+        # if val_y[i].item() in indices[i].tolist():
+        mrr += 1. / (1 + indices[i].tolist().index(output_y[i][-1]))
+        for topn in range(K):
+            if output_y[i][-1] in indices[i][:topn + 1].tolist():
+                hit_n[topn] += 1
+
+    return np.array(hit_n) * 1. / indices.shape[0], mrr / indices.shape[0]
+
+
+# model = GRU4Rec(dp.itemVoc.num_words, 3).to(device)
+# out, h = model(batch_x, batch_x_len)
+# model.ce(out, pack_padded_sequence(torch.tensor(batch_y), batch_x_len, batch_first=True).data)
+
+
+epoch_num = 20
+batch_size = 500
+hidden_size = 50
+lr = 0.05
+
+model = GRU4Rec(dp.itemVoc.num_words, hidden_size).to(device)
+optimizer = optim.Adagrad(model.parameters(), lr)
+
+val_data = DataLoader(val, batch_size=len(val), shuffle=False, collate_fn=collate_fn)
+val_x_iid, val_y_iid, val_y_price, val_seq_len = iter(val_data).next()
+
+test_data = DataLoader(test, batch_size=len(test), shuffle=False, collate_fn=collate_fn)
+test_x_iid, test_y_iid, test_y_price, test_seq_len = iter(test_data).next()
+
+for step in range(epoch_num):
+    data_loader = DataLoader(train, batch_size=batch_size, shuffle=True, collate_fn=collate_fn)
+    for i, min_batch in enumerate(data_loader):
+        model.train()
+
+        batch_x_iid, batch_y_iid, batch_y_price, batch_x_len = min_batch
+        # batch_x_iid = batch_x_iid.to(device)
+        out, h, p = model(batch_x_iid, batch_x_len)
+
+        label_iid = pack_padded_sequence(torch.tensor(batch_y_iid), batch_x_len, batch_first=True).data
+        loss_iid = model.ce(out, label_iid)
+
+        label_price = pack_padded_sequence(torch.tensor(batch_y_price), batch_x_len, batch_first=True).data
+        loss_price = model.mse(p.squeeze(), label_price)
+
+        loss = loss_iid + loss_price
+        # loss = loss_iid
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+        print(loss.item(), i, step)
+        if (i + 1) % 10 == 0:
+            model.eval()
+            evaluation(model, val_x_iid, val_y_iid, val_seq_len, 5)
+            print(evaluation(model, val_x_iid, val_y_iid, val_seq_len, 5))
+
+model.eval()
+print(evaluation(model, test_x_iid, test_y_iid, test_seq_len, 10))
+
+
