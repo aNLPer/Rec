e382a3e47b883f7778635adec1cd640e1342577b
welch <welch_cn@163.com>
2022-10-26 19:23:16 +0800

    train model

--- test.py
+++ test.py
@@ -1,11 +1,5 @@
-import random
-import numpy as np
-import torch
-import torch.nn as nn
-import torch.nn.functional as F
-from torch.distributions import Categorical
 
-a = torch.tensor([[1],[2],[3]])
-b = torch.tensor([[1],[4],[7]])
-print(a.mul(b))
-print(b.mul(a))
+
+a = {"a":[1,2,3,4], "b":[1,3,4,5,6,7]}
+for key,value in a.items():
+    print(a.pop(key))
